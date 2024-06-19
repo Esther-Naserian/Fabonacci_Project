3b5a9010e78d41703ac57503c1e14251323c6253
Esther-Naserian <naseriansamuria@gmail.com>
2024-06-19 14:11:54 +0300

    add files

---
+++ app/src/main/java/com/example/fibonacciproject/MainActivity.kt
@@ -0,0 +1,33 @@
+package com.example.fibonacciproject
+
+import android.os.Binder
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.GridLayoutManager
+import com.example.fibonacciproject.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding =ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        val number = addNumbers(100)
+        binding.tvNumbers.layoutManager=GridLayoutManager(this,2)
+        val adapterNums= SequenceNumbers(number)
+        binding.tvNumbers.adapter=adapterNums
+
+
+    }
+    fun addNumbers(num:Int):List<Int>{
+        val number = mutableListOf(0,1)
+        while (number.size < num){
+            number.add(number[number.lastIndex]+number[number.lastIndex -1])
+        }
+        return number
+    }
+}
---
+++ app/src/main/java/com/example/fibonacciproject/SequenceNumbers.kt
@@ -0,0 +1,31 @@
+package com.example.fibonacciproject
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.RecyclerView.Recycler
+import androidx.recyclerview.widget.RecyclerView.ViewHolder
+
+class SequenceNumbers (var nums:List<Int>):RecyclerView.Adapter<AllFibonacciHolder>(){
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): AllFibonacciHolder {
+        val itemView = LayoutInflater.from (parent.context).inflate(R.layout.sequence,parent,false)
+        return AllFibonacciHolder(itemView)
+
+    }
+
+    override fun onBindViewHolder(holder: AllFibonacciHolder, position: Int) {
+        holder.rvNumbers.text= nums[position].toString()
+    }
+
+
+    override fun getItemCount(): Int {
+        return nums.size
+    }
+
+}
+class AllFibonacciHolder(itemView:View):ViewHolder(itemView){
+    var rvNumbers= itemView.findViewById<TextView>(R.id.rvNumbers)
+
+}
